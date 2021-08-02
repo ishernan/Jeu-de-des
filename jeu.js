b2097799f08be91a3484c3b8470f370935ee9bf0
Isaac Hernandez <isaac.hernandez@outlook.com>
2021-08-02 16:29:28 +0200

    desing anf firts functionalites

--- jeu.js
+++ jeu.js
@@ -1,13 +1,22 @@
-const button = document.querySelector('.btn'); 
-const numero = document.querySelector('#numero'); 
+// const button = document.querySelector('.btn'); 
+// const numero = document.querySelector('#numero'); 
 
 
 
-button.addEventListener('click', ()=>{
-  jeuDe()
-})
+// button.addEventListener('click', ()=>{
+//   jeuDe()
+// })
 
-function jeuDe () { 
-  let de = Math.floor((Math.random() * 6) + 1 ); 
-  numero.textContent = de 
- }
+// function jeuDe () { 
+//   let de = Math.floor((Math.random() * 6) + 1 ); 
+//   numero.textContent = de 
+//  }
+
+const rejouer = document.querySelector('#rejouer'); 
+const de1 = document.querySelector('#de1'); 
+const de2 = document.querySelector('#de2'); 
+
+rejouer.addEventListener('click', ()=>{
+  de1.style.display = 'none'; 
+  de2.style.display = 'none'; 
+} )
