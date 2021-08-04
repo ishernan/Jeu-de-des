ae7733cc59821663c0e8c01a1f1aaa5bd6756a77
Isaac Hernandez <isaac.hernandez@outlook.com>
2021-08-04 16:49:58 +0200

    functions, players

--- jeu.js
+++ jeu.js
@@ -1,22 +1,36 @@
-const rejouer = document.querySelector('#rejouer'); 
-const de1 = document.querySelector('#de1'); 
-const de2 = document.querySelector('#de2'); 
+const rejouer   = document.querySelector('#rejouer'); 
+const de1       = document.querySelector('#de1'); 
+const de2       = document.querySelector('#de2'); 
 const roulezDes = document.querySelector('#roulez-des'); 
-const points1= document.querySelector('#points-1');
-const points2= document.querySelector('#points-2');
+const points1   = document.querySelector('#points-1');
+const points2   = document.querySelector('#points-2');
+const select1   = document.querySelector('#select1');
+const select2   = document.querySelector('#select2');
+let player1, player2;
 
 
 roulezDes.addEventListener('click', ()=>{
   jeuDe(); 
 })
 
-
 rejouer.addEventListener('click', ()=>{
   de1.style.display = 'none'; 
   de2.style.display = 'none'; 
 } )
 
-
+select1.addEventListener('click', ()=>{
+  select1.style.backgroundColor = "#f1a19e"; 
+  select2.style.backgroundColor = "white"; 
+  player1 = true;
+  player2 = false;
+  
+})
+select2.addEventListener('click', ()=>{
+  select2.style.backgroundColor = "#f1a19e"; 
+  select1.style.backgroundColor = "white"; 
+  player2 = true;
+  player1 = false;
+})
 
 function jeuDe () { 
   let rouler1 = Math.floor((Math.random() * 6) + 1 ); 
@@ -26,10 +40,15 @@ function jeuDe () {
   de2.style.display = 'block';
   de1.src = 'images/dice-' + rouler1 + '.png';
   de2.src = 'images/dice-' + rouler2 + '.png';
-  points1.textContent = rouler1; 
-  points2.textContent = rouler2; 
-
+  
+  if(player1 === true ){
+    points1.textContent = rouler1+=rouler2;     
+  } else if(player2===true){
+    points2.textContent = rouler1+=rouler2; 
+    }
   }
+
+  
 
  
  
